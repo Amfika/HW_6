afcda09cd351847b19b4315fe2f15154cbe258d3
ap.sechko <asanastasia1@gmail.com>
2023-06-21 22:15:59 +0300

    with @Test

---
+++ src/test/java/Tests.java
@@ -0,0 +1,21 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Tests {
+
+    @Test
+    public static void firstTest(){
+        System.out.println("First test!");
+    }
+
+    @Test
+    public static void secondTest(){
+        System.out.println("Second test!");
+    }
+
+    @Test
+    public static void failingTest(){
+        System.out.println("failing test");;
+    }
+    }
+
